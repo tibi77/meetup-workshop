fcd2071248cb7cff4e60226530448a805cd0d7c0
Tiberiu Lepădatu <tiberiulepadatu14@gmail.com>
2020-09-06 13:57:00 +0300

    Init

---
+++ server/server.ts
@@ -0,0 +1,15 @@
+/**
+ * Server for our application
+ * Here we will run the express server and will use it with socker.io
+ * to implement WebRTC
+ * 
+ * Language: TypeScript (if you are better in javascript please change the
+ *            extension and also remove tsc from package.json)
+ * 
+ * Libraries that you are going to use:
+ * - express
+ * - socket.io
+ * - http
+*/
+
+/** TODO 1: Create server class and import it in the index.ts */
